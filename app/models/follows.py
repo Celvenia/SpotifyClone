3a541fd0068d1b80b0ff108a1e0935af3fd7fb4b
leongkevin <99384279+leongkevin@users.noreply.github.com>
2023-05-02 22:20:59 -0400

    All models are created

---
+++ app/models/follows.py
@@ -0,0 +1,11 @@
+from .db import db, environment, SCHEMA
+
+class Follow(db.Model):
+    __tablename__ = 'follows'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    following_id = db.Column(db.Integer, nullable=False)
